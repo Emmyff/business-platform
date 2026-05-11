924e7004214ad59619ac996f21e8745501445acb
Ryoiki <emmanueltalabi97@gmail.com>
2026-05-11 11:42:58 +0100

    fix: install supabase dependency

--- src/supabase.js
+++ src/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = 'https://alkmqpdgqadoetxwrrsq.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFsa21xcGRncWFkb2V0eHdycnNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0ODU1NTUsImV4cCI6MjA5NDA2MTU1NX0.-l1wvdPK2IMPUsU7QJiKjV5CRKmPGz8lGJ2ZfdWebbQ'
